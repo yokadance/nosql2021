2583270f6f40a00dd604e2433e915fb8203e73b3
Juan Mussini <juan5028@hotmail.com>
2021-10-29 20:37:36 -0300

    iniciando

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,23 +1,12 @@
 package com.example.demo;
 
-import com.example.demo.model.Car;
-import com.example.demo.model.CarType;
 import com.example.demo.model.Person;
 import com.example.demo.model.UserContext;
-import com.example.demo.repository.CarRepository;
 import com.example.demo.repository.PersonRepository;
-import com.google.auth.oauth2.GoogleCredentials;
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.FirebaseOptions;
-import com.google.firebase.auth.FirebaseAuth;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
-import java.io.FileInputStream;
-import java.io.InputStream;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -28,16 +17,14 @@ import org.springframework.web.context.annotation.RequestScope;
 @SpringBootApplication
 @OpenAPIDefinition(
   info = @Info(
-    title = "Sample Spring Boot API",
+    title = "NoSQL 2021",
     version = "v1",
-    description = "A demo project using Spring Boot with Swagger-UI enabled"
+    description = "Obligatorio 2 NoSQL 2021"
   )
 )
 @SecurityScheme(name = "bearerAuth", type = SecuritySchemeType.HTTP, bearerFormat = "JWT", scheme = "bearer")
-@EnableNeo4jAuditing(modifyOnCreate = true, setDates = true)
+@EnableNeo4jAuditing()
 public class DemoApplication {
-
-  private static final Logger log = LoggerFactory.getLogger(DemoApplication.class);
 
   public static void main(String[] args) {
     SpringApplication.run(DemoApplication.class, args);
@@ -50,10 +37,10 @@ public class DemoApplication {
   }
 
   @Bean
-  CommandLineRunner demo(PersonRepository personRepository, CarRepository carRepository) {
+  CommandLineRunner demo(PersonRepository personRepository) {
     return args -> {
       personRepository.deleteAll();
-      carRepository.deleteAll();
+
 
       var greg = new Person("Greg", "greg@greg.com", "test123");
       var roy = new Person("Roy","roy@roy.com", "test123");
@@ -63,22 +50,6 @@ public class DemoApplication {
       personRepository.save(roy);
       personRepository.save(craig);
 
-      //var x = personRepository.findGreg();
-      //log.info("Este es GREG... {}", x.getCreatedAt());
-
-   /*   greg.worksWith(roy);
-      personRepository.save(greg);
-
-      var auto = new Car("Nose que", 4, CarType.SEDAN);
-      carRepository.save(auto);
-      greg.addCar(auto);
-      personRepository.save(greg);
-*/
-      var autodos = carRepository.findByMake("Nose que");
-      if (autodos.isPresent()) {
-        var autodosposta = autodos.get();
-        System.out.println("");
-      }
     };
   }
 }
--- src/main/java/com/example/demo/repository/RoleRepository.java
+++ src/main/java/com/example/demo/repository/RoleRepository.java
@@ -1,6 +1,6 @@
 package com.example.demo.repository;
 
-import com.example.demo.model.Car;
+
 import com.example.demo.model.Person;
 import com.example.demo.model.Role;
 import java.util.Optional;
